7d63906273bfc6ed35e9926d8e5a060274c49783
pidrmasiin <petteri.paakkonen@helsinki.fi>
2018-08-21 17:34:27 +0300

    rest

--- src/main/java/storage/controllers/DefaultController.java
+++ src/main/java/storage/controllers/DefaultController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import storage.domain.Item;
 import storage.repository.ItemRepository;
@@ -30,7 +31,7 @@ public class DefaultController {
            this.itemRepository.save(item);
         }
 
-        return "redirect:/";
+        return "redirect:/index";
     }
 
 }
---
+++ src/main/java/storage/controllers/ItemRestController.java
@@ -0,0 +1,22 @@
+package storage.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import storage.domain.Item;
+import storage.repository.ItemRepository;
+
+@RestController
+@RequestMapping("items")
+public class ItemRestController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @RequestMapping(method=RequestMethod.GET)
+      public List<Item> getBooks() {
+        return itemRepository.findAll();
+    }
+}

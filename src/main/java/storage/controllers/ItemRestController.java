8ceac54aed42a0d9135697a9a0307c9def89a3fa
pidrmasiin <petteri.paakkonen@helsinki.fi>
2018-08-21 17:41:24 +0300

    crossOrgin

--- src/main/java/storage/controllers/ItemRestController.java
+++ src/main/java/storage/controllers/ItemRestController.java
@@ -2,11 +2,13 @@ package storage.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import storage.domain.Item;
 import storage.repository.ItemRepository;
+
 
 @RestController
 @RequestMapping("items")
@@ -14,7 +16,8 @@ public class ItemRestController {
 
     @Autowired
     private ItemRepository itemRepository;
-
+    
+    @CrossOrigin(origins = "/**")
     @RequestMapping(method=RequestMethod.GET)
       public List<Item> getBooks() {
         return itemRepository.findAll();
